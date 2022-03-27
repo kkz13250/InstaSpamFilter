9eabadac94f846271c360601d69d0f22f7c8b8f6
JulieZhang0102 <jzhang10@babson.edu>
2022-03-27 18:53:58 -0400

    fetch comments data from ins API

---
+++ comments.py
@@ -0,0 +1,37 @@
+from apify_client import ApifyClient
+import csv
+
+# Initialize the ApifyClient with your API token
+client = ApifyClient("apify_api_rM9VBaqLfbBYFURfsehjpWUxvBj1lJ2uWZ3v")
+
+# Prepare the actor input
+run_input = {
+    "directUrls": [
+        "https://www.instagram.com/p/CbihreevAfa/",
+    ],
+    "resultsLimit": 10,
+}
+
+# Run the actor and wait for it to finish
+run = client.actor("zuzka/instagram-comment-scraper").call(run_input=run_input)
+
+# Fetch and print actor results from the run's dataset (if there are any)
+data = []
+for item in client.dataset(run["defaultDatasetId"]).iterate_items():
+    comment_info = {}
+    comment_info['comment_id'] = item['id']
+    comment_info['post_id'] = item['postId']
+    comment_info['comment_content'] = item['text'].replace("\n", ". ")
+    comment_info['commenter_username'] = item['ownerUsername']
+    comment_info['commenter_pic_url'] = item['ownerProfilePicUrl']
+    data.append(comment_info)
+
+print(data)
+
+csv_header = ['comment_id', 'post_id', 'comment_content',
+              'commenter_username', 'commenter_pic_url']
+
+with open('comments.csv', 'w', encoding='utf-8', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=csv_header)
+    writer.writeheader()
+    writer.writerows(data)
