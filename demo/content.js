5e0dfeb994ab7c4fdd9a11116a22284cf298570c
kevinkzCanada <kevinkz@umich.edu>
2022-03-29 20:58:06 -0600

    highlight given timestamps

--- demo/content.js
+++ demo/content.js
@@ -1,27 +1,37 @@
 
 console.log("Chrome extension go");
 
-var timeSet = ["2022-03-29T20:22:31.000Z"];
+var timeSet = new Set();
+timeSet.add("2022-03-29T20:22:31.000Z")
+console.log(timeSet)
+document.addEventListener('click', function (e) {
+    e.target.style['background-color'] = '#FF0000';
+});
 
 //timeout to wait for the website to finish loading all the comments
 const myTimeout = setTimeout(function() {
     let comments = document.getElementsByClassName('Mr508 ');
     console.log(comments);
     for (elt of comments){
-        elt.style['background-color'] = '#FF0000';
-        markedTimestamp(elt);
-        console.log(elt);
+        var ret = markedTimestamp(elt);
+        console.log(ret);
+        if (ret){
+            elt.style['background-color'] = '#FF0000';
+        }
+        //console.log(elt);
 }}, 5000);
 
 function markedTimestamp(tree) {
     var all = tree.getElementsByTagName("*");
-
     for (var i=0, max=all.length; i < max; i++) {
         const result = all[i].hasAttribute('datetime');
         if (result) {
-            if (timeSet.has(all[i].datetime)){
-                console.log(all[i]);
+            var dt = String(all[i].getAttribute("datetime"));
+            if (timeSet.has(dt)){
+                //console.log(all[i]);
+                return true;
             }
         }
     }
+    return false;
 }
