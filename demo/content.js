84f1b2674ba281d5c359bd732b0709d2db31f8cb
kevinkzCanada <kevinkz@umich.edu>
2022-03-30 05:29:19 -0600

    working demo

--- demo/content.js
+++ demo/content.js
@@ -2,31 +2,70 @@
 console.log("Chrome extension go");
 
 var timeSet = new Set();
-timeSet.add("2022-03-29T20:22:31.000Z")
+timeSet.add("2021-12-29T19:47:01.000Z")
+timeSet.add("2021-09-09T18:58:44.000Z")
+timeSet.add("2021-08-22T18:08:26.000Z")
+timeSet.add("2021-07-03T13:59:38.000Z")
+timeSet.add("2021-06-03T02:27:32.000Z")
+timeSet.add("2021-04-14T02:11:11.000Z")
+timeSet.add("2021-04-10T07:05:34.000Z")
+timeSet.add("2021-03-28T20:36:56.000Z")
+timeSet.add("2021-03-25T22:28:08.000Z")
+timeSet.add("2021-03-18T14:31:14.000Z")
 console.log(timeSet)
-document.addEventListener('click', function (e) {
-    var color = e.target.style['background-color'];
-    console.log(color)
-    if (color == "rgb(255, 0, 0)") {
-        console.log(e.target)
-        e.target.removeAttribute("style");;
-    } else{
-        e.target.style['background-color'] = '#FF0000';
-    }
-});
 
-//timeout to wait for the website to finish loading all the comments
-const myTimeout = setTimeout(function() {
-    let comments = document.getElementsByClassName('Mr508 ');
-    console.log(comments);
-    for (elt of comments){
-        var ret = markedTimestamp(elt);
-        console.log(ret);
-        if (ret){
-            elt.style['background-color'] = '#FF0000';
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (request.text == "markSpam") {
+        var counter = 0
+        console.log('requestReceived');
+        document.addEventListener('click', function (e) {
+            if (counter < 1){
+                //travese back up the DOM tree to find the comment class
+                var currElement = e.target;
+                console.log(currElement.className)
+                while (currElement.className != 'Mr508 '){
+                    currElement = currElement.parentElement;
+                }
+                console.log(currElement.className)
+                if (currElement.style['background-color'] == "rgb(255, 0, 0)") {
+                    console.log(currElement);
+                    currElement.removeAttribute("style");
+                    deletefromSet(currElement);
+                } else{
+                    currElement.style['background-color'] = '#FF0000';
+                    addtoSet(currElement);
+                }
+                counter += 1;
+            }
+            return
+        });
+      } else if (request.text == "switchOnOff"){
+            let comments = document.getElementsByClassName('Mr508 ');
+            console.log(comments);
+            for (elt of comments){
+                var ret = markedTimestamp(elt);
+                console.log(ret);
+                if (ret && elt.style['background-color'] != "rgb(255, 0, 0)"){
+                    elt.style['background-color'] = '#FF0000';
+                } else {
+                    elt.removeAttribute("style");;
+                }
+                //console.log(elt);
+            }
+        }  else if (request.text == "hl"){
+            console.log("highlight requested");
+            window.location.reload();
+        } else if (request.text == "hd"){
+            console.log("hide requested");
+            let comments = document.getElementsByClassName('Mr508 ');
+            for (elt of comments){
+                if (elt.style['background-color'] == "rgb(255, 0, 0)"){
+                    elt.remove();
+                }
+            }
         }
-        //console.log(elt);
-}}, 5000);
+  });
 
 function markedTimestamp(tree) {
     var all = tree.getElementsByTagName("*");
@@ -41,4 +80,28 @@ function markedTimestamp(tree) {
         }
     }
     return false;
+}
+
+function addtoSet(tree) {
+    var all = tree.getElementsByTagName("*");
+    for (var i=0, max=all.length; i < max; i++) {
+        const result = all[i].hasAttribute('datetime');
+        if (result) {
+            var dt = String(all[i].getAttribute("datetime"));
+            timeSet.add(dt);
+            return
+        }
+    }
+}
+
+function deletefromSet(tree) {
+    var all = tree.getElementsByTagName("*");
+    for (var i=0, max=all.length; i < max; i++) {
+        const result = all[i].hasAttribute('datetime');
+        if (result) {
+            var dt = String(all[i].getAttribute("datetime"));
+            timeSet.delete(dt);
+            return
+        }
+    }
 }
--- demo/index.js
+++ demo/index.js
@@ -1,11 +1,47 @@
-document.getElementById("report").addEventListener("click", myFunction);
+var el = document.getElementById("report");
+if(el){
+  el.addEventListener("click", myFunction);
+}
 
 function myFunction(){
     window.location.href = "report.html";
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {text:"markSpam"});
+    });
   }
 
-document.getElementById("sw").addEventListener("click", changeStyle);
+var el2 = document.getElementById("sw");
+if(el2){
+  el2.addEventListener("click", changeStyle);
+}
+
 function changeStyle(){
-  var comments = document.getElementById("report")
-  comments.innerHTML = "hello world"
+  console.log("switch triggered");
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {text:"switchOnOff"});
+  });
+} 
+
+var el3 = document.getElementById("fselect");
+if(el3){
+  el3.addEventListener("change", highlightOp);
+}
+
+function highlightOp(){
+  console.log("highlight selected");
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {text:"highlight"});
+  });
+} 
+
+var el4 = document.getElementById("hd");
+if(el4){
+  el4.addEventListener("click", hideOp);
+}
+
+function hideOp(){
+  console.log("switch triggered");
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {text:"hide"});
+  });
 } 
