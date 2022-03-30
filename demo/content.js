79ff433d94f1d649ff57dbd729402b9759acae1f
kevinkzCanada <kevinkz@umich.edu>
2022-03-29 22:16:08 -0600

    updated select/unselect highlights

--- demo/content.js
+++ demo/content.js
@@ -5,7 +5,14 @@ var timeSet = new Set();
 timeSet.add("2022-03-29T20:22:31.000Z")
 console.log(timeSet)
 document.addEventListener('click', function (e) {
-    e.target.style['background-color'] = '#FF0000';
+    var color = e.target.style['background-color'];
+    console.log(color)
+    if (color == "rgb(255, 0, 0)") {
+        console.log(e.target)
+        e.target.removeAttribute("style");;
+    } else{
+        e.target.style['background-color'] = '#FF0000';
+    }
 });
 
 //timeout to wait for the website to finish loading all the comments
