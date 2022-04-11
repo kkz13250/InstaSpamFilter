74104a2e4960d6f6004b9ef84803c6179e491527
kevinkzCanada <kevinkz@umich.edu>
2022-04-11 04:33:43 -0600

    ver 1.0.1 with updated sinterface and frontend/backend integration

--- demo/content.js
+++ demo/content.js
@@ -2,17 +2,6 @@
 console.log("Chrome extension go");
 
 var timeSet = new Set();
-timeSet.add("2021-12-29T19:47:01.000Z")
-timeSet.add("2021-09-09T18:58:44.000Z")
-timeSet.add("2021-08-22T18:08:26.000Z")
-timeSet.add("2021-07-03T13:59:38.000Z")
-timeSet.add("2021-06-03T02:27:32.000Z")
-timeSet.add("2021-04-14T02:11:11.000Z")
-timeSet.add("2021-04-10T07:05:34.000Z")
-timeSet.add("2021-03-28T20:36:56.000Z")
-timeSet.add("2021-03-25T22:28:08.000Z")
-timeSet.add("2021-03-18T14:31:14.000Z")
-console.log(timeSet)
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
@@ -28,12 +17,12 @@ chrome.runtime.onMessage.addListener(
                     currElement = currElement.parentElement;
                 }
                 console.log(currElement.className)
-                if (currElement.style['background-color'] == "rgb(255, 0, 0)") {
+                if (currElement.style['background-color'] == "rgb(169, 169, 169)") {
                     console.log(currElement);
                     currElement.removeAttribute("style");
                     deletefromSet(currElement);
                 } else{
-                    currElement.style['background-color'] = '#FF0000';
+                    currElement.style['background-color'] = '#A9A9A9';
                     addtoSet(currElement);
                 }
                 counter += 1;
@@ -46,10 +35,10 @@ chrome.runtime.onMessage.addListener(
             for (elt of comments){
                 var ret = markedTimestamp(elt);
                 console.log(ret);
-                if (ret && elt.style['background-color'] != "rgb(255, 0, 0)"){
-                    elt.style['background-color'] = '#FF0000';
+                if (ret && request.status == "true"){
+                    elt.style['background-color'] = '#A9A9A9';
                 } else {
-                    elt.removeAttribute("style");;
+                    elt.removeAttribute("style");
                 }
                 //console.log(elt);
             }
@@ -60,10 +49,19 @@ chrome.runtime.onMessage.addListener(
             console.log("hide requested");
             let comments = document.getElementsByClassName('Mr508 ');
             for (elt of comments){
-                if (elt.style['background-color'] == "rgb(255, 0, 0)"){
+                if (elt.style['background-color'] == "rgb(169, 169, 169)"){
                     elt.remove();
                 }
             }
+        } else if (request.text == "timestamps"){
+            var data = request.data;
+            for (var i = 0; i < data.length; i++){
+                if (data[i].PREDICTION == "1"){
+                    timeSet.add(data[i].timestamp);
+                }
+            }
+            console.log("HERE")
+            console.log(timeSet)
         }
   });
 
--- demo/index.js
+++ demo/index.js
@@ -10,34 +10,27 @@ function myFunction() {
   });
 }
 
+
 var el2 = document.getElementById("sw");
 if (el2) {
   el2.addEventListener("click", changeStyle);
 }
+var onOff = localStorage.getItem("sw");
+if (onOff == "true") {
+  el2.checked = true;
+}
 
 function changeStyle() {
   console.log("switch triggered");
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
-    let url = tabs[0].url;
-    try {
-      let url_id = String(url).split("/p/")[1].slice(0, -1);
-      // alert(url_id);
-      fetch("http://3.231.28.43:5000/api/getURL?api_url=" + url_id, {
-        mode: "cors",
-        method: "GET",
-      })
-        .then((response) => {
-          return response.json(); // server returned valid JSON
-        })
-        .then((parsed_result) => {
-          console.log(parsed_result);
-        });
-    } catch (err) {
-      alert("No post detected, please open a specific instagram post");
-    }
-  });
+  var onOff = localStorage.getItem("sw");
+  if(onOff == "true"){
+    localStorage.setItem("sw", "false");
+  }else{
+    localStorage.setItem("sw", "true");
+  }
+  onOff = localStorage.getItem("sw");
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { text: "switchOnOff" });
+    chrome.tabs.sendMessage(tabs[0].id, { text: "switchOnOff", status: onOff});
   });
 }
 
@@ -64,3 +57,32 @@ function hideOp() {
     chrome.tabs.sendMessage(tabs[0].id, { text: "hide" });
   });
 }
+
+console.log("start query");
+var ini = true;
+if (ini){
+  ini = false;
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+    let url = tabs[0].url;
+    try {
+      let url_id = String(url).split("/p/")[1].slice(0, -1);
+      // alert(url_id);
+      fetch("http://3.231.28.43:5000/api/getURL?api_url=" + url_id, {
+        mode: "cors",
+        method: "GET",
+      })
+        .then((response) => {
+          return response.json(); // server returned valid JSON
+        })
+        .then((parsed_result) => {
+          console.log(parsed_result);
+          chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { text: "timestamps", data: parsed_result});
+          });
+        });
+    } catch (err) {
+      alert("No post detected, please open a specific instagram post");
+    }
+  });
+}
+console.log("end query");
